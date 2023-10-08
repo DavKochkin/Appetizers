6091b2f6c9ab08abce76ed7aeea779c584e4280e
David Kochkin <kochkin.david@gmail.com>
2023-10-08 09:16:12 +0300

    Add AlertItem

---
+++ Appetizers/Alert.swift
@@ -0,0 +1,20 @@
+//
+//  Alert.swift
+//  Appetizers
+//
+//  Created by David Kochkin on 08.10.2023.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id: UUID
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContex {
+    
+    
+}
