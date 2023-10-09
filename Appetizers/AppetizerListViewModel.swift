e3fee7cb1e62ae4fc554c7397f1063ef66c176e8
David Kochkin <kochkin.david@gmail.com>
2023-10-09 22:28:34 +0300

    Error Handling

--- Appetizers/AppetizerListViewModel.swift
+++ Appetizers/AppetizerListViewModel.swift
@@ -10,15 +10,29 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
+                    
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContex.invalidResponse
+                        
+                    case .invalidURL:
+                        alertItem = AlertContex.invalidURL
+                        
+                    case .invalidData:
+                        alertItem = AlertContex.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContex.unableComplete
+                    }
                 }
             }
         }
