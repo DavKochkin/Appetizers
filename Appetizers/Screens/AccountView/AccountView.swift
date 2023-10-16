5d1e1c080820868e9c5fe5f589a8618314de2fa7
David Kochkin <kochkin.david@gmail.com>
2023-10-16 21:02:04 +0300

    Add AccountView Personal Info

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthday = Date()
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled()
+                    DatePicker("Birthday", selection: $birthday,
+                               displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+            }
+            .navigationTitle("Account")
         }
     }
 }
