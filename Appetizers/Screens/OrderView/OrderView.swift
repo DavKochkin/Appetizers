1529dfbdab629620708bc7ece1b07801b4a53cb4
David Kochkin <kochkin.david@gmail.com>
2023-10-21 09:14:45 +0300

    Refactor OrderView

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -9,41 +9,26 @@ import SwiftUI
 
 struct OrderView: View {
     
-    @StateObject var viewModel = AppetizerListViewModel()
-    
     var body: some View {
         ZStack {
             NavigationView {
                 VStack {
                     List {
-                        orderViewList(image: "asian-flank-steak",
-                                      name: "Asian Flank Steak",
-                                      price: "$15.99")
-                        orderViewList(image: "asian-flank-steak",
-                                      name: "Asian Flank Steak",
-                                      price: "$15.99")
-                        orderViewList(image: "asian-flank-steak",
-                                      name: "Asian Flank Steak",
-                                      price: "$15.99")
-                        
-                    }
-                    VStack {
-                        Button {
-                            print("Save")
-                        } label: {
-                            Text("$15.99 - Place Order")
-                                .font(.title3)
-                                .fontWeight(.semibold)
-                                .frame(width: 350, height: 50)
-                                .foregroundStyle(.white)
-                                .background(Color(.brandPrimary))
-                                .clipShape(.buttonBorder)
+                        ForEach(MockData.orderItems) { appetizer in
+                            AppetizerListCell(appetizer: appetizer)
                         }
-                        .padding(.bottom, 20)
                     }
+                    .listStyle(PlainListStyle())
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        APButton(title: "$15.99 - Place Order")
+                    }
+                    .padding(.bottom, 20)
+                    
                 }
                 .navigationTitle("Order")
-                .listStyle(PlainListStyle())
             }
         }
     }
@@ -53,23 +38,3 @@ struct OrderView: View {
     OrderView()
 }
 
-struct orderViewList: View {
-    
-    let image: String
-    let name: String
-    let price: String
-    
-    var body: some View {
-        HStack {
-            Image(image)
-                .resizable()
-                .frame(width: 80, height: 80)
-            VStack(alignment: .leading, spacing: 5) {
-                Text(name)
-                    .font(.title3)
-                Text(price)
-            }
-            .padding(.leading)
-        }
-    }
-}
